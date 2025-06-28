f6194e888c1d0d89c45e0939a9e00975918bfc67
TheNineToday <157670353+TheNineToday@users.noreply.github.com>
2025-06-28 09:46:50 +0700

    Add files via upload

---
+++ Bel_Rumah_ANTI_MAINSTREAM.ino
@@ -0,0 +1,14 @@
+const int buttonPin = 26;
+const int buzzerPin = 27;
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(buzzerPin, HIGH);
+  } else {
+    digitalWrite(buzzerPin, LOW);
+  }
+}
